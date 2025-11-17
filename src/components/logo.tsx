c0144d701a18c87cacfe29de7f1fb765645ce51e
İbrahim <ibrahimcanonder9895@gmail.com>
2025-11-17 23:23:17 +0000

    png olarak

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,10 +5,10 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-2", className)}>
       <Image 
-        src="https://i.ibb.co/mR5pwn6/logo.png" 
+        src="https://i.ibb.co/6yVpWfQ/turk-cocuk-akademisi-logo.png" 
         alt="Türk Çocuk Akademisi Logo" 
-        width={140} 
-        height={35} 
+        width={50} 
+        height={50} 
         className="w-auto"
       />
     </div>
