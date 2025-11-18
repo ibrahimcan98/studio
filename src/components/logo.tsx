b47546070c8bec14347f3d8cce76fdcb48b567f3
İbrahim <ibrahimcanonder9895@gmail.com>
2025-11-18 13:55:57 +0000

    h-20 yapalim (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-2 font-bold text-lg", className)}>
-      <Image src="/logo.png" alt="Türk Çocuk Akademisi Logo" width={55} height={55} className="h-16 w-auto" />
+      <Image src="/logo.png" alt="Türk Çocuk Akademisi Logo" width={55} height={55} className="h-20 w-auto" />
     </div>
   );
 }
