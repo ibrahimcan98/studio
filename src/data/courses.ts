e5de1a4d1fd2f1e30bde8ed2032acdba0fa0e9bd
İbrahim <ibrahimcanonder9895@gmail.com>
2025-11-18 22:30:35 +0000

    bunun rengini #C9CBCB yap (_for element <Badge>_)

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -84,8 +84,8 @@ export const COURSES: Course[] = [
             text: "Detaylı bilgi için tıkla >",
             iconBgColor: "bg-teal-100",
             iconTextColor: "text-teal-500",
-            badgeColor: "bg-orange-400",
-            badgeTextColor: "text-white",
+            badgeColor: "bg-[#C9CBCB]",
+            badgeTextColor: "text-gray-800",
             linkTextColor: "text-orange-500",
         },
         details: {
