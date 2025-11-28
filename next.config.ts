35662568f73e159ec56603c9b5886fd5051a18cb
İbrahim <ibrahimcanonder9895@gmail.com>
2025-11-28 07:41:59 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // This allows the Next.js development server to accept requests from the
-    // Firebase Studio environment.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
