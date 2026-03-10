689c44e473bd80206232518668774bd5b85c2fad
İbrahim <ibrahimcanonder9895@gmail.com>
2026-03-10 09:36:28 +0000

    hayir favicon.png olarak degistir applede aynen degistir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export const metadata: Metadata = {
   title: 'Türk Çocuk Akademisi',
   description: 'Yurt dışındaki çocuklar için oyun tabanlı, eğlenceli ve etkili Türkçe öğrenimi platformu.',
   icons: {
-    icon: '/favicon.ico',
-    apple: '/logo.png',
+    icon: '/favicon.png',
+    apple: '/favicon.png',
   },
 };
 
