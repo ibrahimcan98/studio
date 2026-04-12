484d1baf961975cc5b8825f588175106047ebf3c
Ibrahim Can ONDER <ibrahimcanonder_98@hotmail.com>
2026-04-12 14:17:55 +0100

    fix: include both google verification tokens for redundancy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,10 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: 'aH6cidLzqSi1MK4imlkOk-c-tOOoGizvIGsCmOysixw',
+    google: [
+      'aH6cidLzqSi1MK4imIkOk-c-tOOoGizvlGsCm35sEsk',
+      'aH6cidLzqSi1MK4imlkOk-c-tOOoGizvIGsCmOysixw'
+    ],
   },
 };
 
